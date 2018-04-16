cb9c337a279b7ac72a9ae72b27ea577e42eb4140
Ken Sodetz <kensodetz@gmail.com>
2018-04-15 20:50:53 -0400

    change names of certain classes

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Home.css';
 
-class App extends Component {
+class Home extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="Home">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Cinnematch</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/Home.js</code> and save to reload.
         </p>
       </div>
     );
   }
 }
 
-export default App;
+export default Home;

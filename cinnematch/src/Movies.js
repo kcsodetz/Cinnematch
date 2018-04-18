d88dae4ac1b342375e1336d12300279bc04bfa55
Cyrus Santiago <santiac@purdue.edu>
2018-04-17 22:34:44 -0500

    added drop down menu

--- cinnematch/src/Movies.js
+++ cinnematch/src/Movies.js
@@ -1,5 +1,10 @@
 import React from 'react'
-
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+const options = [
+  '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'
+]
+const defaultOption = options[0]
 class Movies extends React.Component{
   render() {
     return(
@@ -7,6 +12,7 @@ class Movies extends React.Component{
         <header>
           <h1 className="Center">Rate Movies</h1>
         </header>
+        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option">  </Dropdown>
       </div>   
     );
   }

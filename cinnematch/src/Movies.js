90534531488a5a8dff883dc03fea8e62dfd679a1
Ken Sodetz <kensodetz@gmail.com>
2018-04-23 19:00:20 -0400

    Begin parsing and getting info from json object

--- cinnematch/src/Movies.js
+++ cinnematch/src/Movies.js
@@ -21,25 +21,6 @@ class Movies extends React.Component{
   }
 
   handleClick = () => {
-    // const request = require("request");
-    // var url =
-    // "https://api.themoviedb.org/3/search/movie?api_key=d75d2433d1369590a08680adda987f45&query=";
-      
-    // url += this.state.value;
-      
-    //  request.get(url, (error, response, body) => {
-    //   let json = JSON.parse(body);
-    //   try {
-    //     console.log(
-    //       json.results[0]
-    //     );
-    //   } catch (error) {
-    //     console.log(
-    //       error.toString()
-    //     );  
-    //   } 
-    //  });
-    
     getMovieInfo(this.state.value);
   }
 
@@ -55,13 +36,12 @@ class Movies extends React.Component{
               type="text"
               name="movieName"
               placeholder="Enter the name of your movie"
-              //value={this.state.value}
               onChange={this.handleInput}
               />
           </p>
         </form>
         <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option">  </Dropdown>
-        <button type="submit" id="addMovieButton" onClick={this.handleClick}> Add Movie </button>
+        <button type="submit" id="addMovieButton" onClick={this.handleClick}> Search </button>
         <div className="row">
           <div className="medium-8 medium-offset-2 column">
             <ul id="flick-list">
--- cinnematch/src/api-request.js
+++ cinnematch/src/api-request.js
@@ -1,14 +1,30 @@
+/**
+ * Returns json object if a movie with a matching title is found. Else, returns 0
+ * 
+ * @author: Ken Sodetz
+ * @param {string} title The title of the movie   
+ */
 export function getMovieInfo(title) {
     const request = require("request");
     var url =
     "https://api.themoviedb.org/3/search/movie?api_key=d75d2433d1369590a08680adda987f45&query=";
 
-        url += title;
-
+    // append title to search query
+    url += title;
+    var json;
     request.get(url, (error, response, body) => {
-    let json = JSON.parse(body);
-    console.log(
-        json.results[0]
-    );
+        json = JSON.parse(body);
+        console.log(
+            json.results[0]
+        );
+
+        //window.alert(json.results[0].title);
+       if (json.total_results == 0) {
+            window.alert("Sorry, your search for " + title + " turned up no results")
+            return 0;
+       }
+       else {
+            return json;    
+       }
     });
 }

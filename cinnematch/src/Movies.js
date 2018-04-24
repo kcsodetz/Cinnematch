4816a18cb4da8a514bd5b41dc513b4a5a5463a7b
Connor Todd <todd37@purdue.edu>
2018-04-23 22:55:22 -0400

    Add API object to state in Movies.js

--- cinnematch/src/Movies.js
+++ cinnematch/src/Movies.js
@@ -8,20 +8,35 @@ const options = [
 
 const defaultOption = options[0]
 class Movies extends React.Component{
+
   constructor(props) {
-    super(props);
-    this.state = {value: ''}
-    this.handleInput = this.handleInput.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    super(props)
+    this.state = {
+      json: {},
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleInput = (event) => {
-    const value = event.target.value;
-    this.setState({value})    
+  handleChange(event) {
+    event.preventDefault()
+    console.log(event.target.movieName.value)
+    const movieName = event.target.movieName.value
+    this.fetchData(movieName)
+    
+    // this.setState({
+    //     movie: event.target.movieName.value,
+    //     json: getMovieInfo(movieName),
+    // })
+    
+    console.log(getMovieInfo(movieName))
   }
 
-  handleClick = () => {
-    getMovieInfo(this.state.value);
+  fetchData(props) {
+    var url = `https://api.themoviedb.org/3/search/movie?api_key=d75d2433d1369590a08680adda987f45&query=`;
+    url += props
+    fetch(url)
+      .then(response => response.json())
+      .then(json => this.setState({ json }))
   }
 
   render() {
@@ -30,26 +45,34 @@ class Movies extends React.Component{
         <header>
           <h1 className="Center">Rate Movies</h1>
         </header>
-        <form>
-          <p>
-            <input
-              type="text"
-              name="movieName"
-              placeholder="Enter the name of your movie"
-              onChange={this.handleInput}
-              />
-          </p>
+        <form id="movie-form" ref={this.form} onSubmit={this.handleChange}>
+          <div className="input-group">
+            <label htmlFor="movieName">
+                <input
+                  type="text"
+                  ref='movie-name-ref'
+                  className="input-group field"
+                  name="movieName"
+                  placeholder="Enter the name of your movie"
+                  required
+                  autoFocus
+                />
+                </label>
+                
+          </div>
+          <br></br>
+          <input type="submit" value="Submit" onSubmit={this.handleChange} />
         </form>
-        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option">  </Dropdown>
-        <button type="submit" id="addMovieButton" onClick={this.handleClick}> Search </button>
         <div className="row">
           <div className="medium-8 medium-offset-2 column">
-            <ul id="flick-list">
+            <ul id="movie-list" ref={this.list}>
             </ul>
           </div>
+          <Dropdown options={options} onChange={this._onSelect} name="year" value={defaultOption} placeholder="Select an rating">  </Dropdown>
+
         </div>
       </div>   
-    )
+    );
   }
 }
 

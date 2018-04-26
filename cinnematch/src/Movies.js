b8a7df0476ceb7a2f3dd193d3f543c1ef2744379
Connor Todd <todd37@purdue.edu>
2018-04-26 01:48:36 -0400

    Now we can rate movies

--- cinnematch/src/Movies.js
+++ cinnematch/src/Movies.js
@@ -33,6 +33,7 @@ class Movies extends React.Component{
     this.handleChange = this.handleChange.bind(this);
     this.addMovie = this.addMovie.bind(this)
     this.getRealName = this.getRealName.bind(this)
+    this.rateMovie = this.rateMovie.bind(this)
   }
 
   /**
@@ -68,6 +69,16 @@ class Movies extends React.Component{
       state: 'movies',
     });
     this.loadProfile()
+  }
+
+  rateMovie(ev){
+    ev.preventDefault()
+    let movie = {...this.state.movie};
+    console.log(ev.target.movieRating.value)
+    movie['rating'] = ev.target.movieRating.value
+    this.setState({
+      movie
+    })
   }
 
   /**
@@ -178,28 +189,30 @@ class Movies extends React.Component{
           {this.state.showComponenet ? 
           <div>
           <button className="AddButton w3-goldenrod" onClick={this.addMovie}>Add to My Movies</button>
-          <form id="movie-rating-form" >
-          <div className="input-group">
-            <label htmlFor="movieName">
-                <input
-                  type="text"
-                  ref='movie-rating-form'
-                  className="input-group field"
-                  name="movieRating"
-                  placeholder="Rate Movie Here"
-                  required
-                  autoFocus
-                />
-                </label>
-                
-          </div>
-            <br></br>
-           
-          </form>
+          
             <MovieDisplay props={this.state.movie}/></div> :
               null
           }
         </form>
+        {this.state.showComponenet ?
+        <form id="movie-rating-form" onSubmit={this.rateMovie} >
+        <div className="input-group">
+          <label htmlFor="movieRating">
+              <input
+                type="text"
+                ref='movie-rating-form'
+                className="input-group field"
+                name="movieRating"
+                placeholder="Rate Movie Here"
+                required
+                autoFocus
+              />
+          </label>
+          <input type="submit" value="Rate Movie" />
+        </div>
+          <br></br>
+        </form> : null }
+      
         <div className="row">
           <div className="medium-8 medium-offset-2 column">
             <ul id="movie-list" ref={this.list}>

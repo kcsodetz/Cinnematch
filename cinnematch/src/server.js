45d8b3859df9979d56ee6f4127d1bb438ec53433
kcsodetz <kensodetz@gmail.com>
2018-04-26 03:21:36 -0400

    server.js testing

---
+++ cinnematch/src/server.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env nodejs
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3000, 'localhost');
+console.log('Server running at http://localhost:3000/');

9708fb9f429bfed13f23cb1b34898991ca41077d
kcsodetz <kensodetz@gmail.com>
2018-04-26 01:52:37 -0400

    Progress on In Theaters functionality

--- cinnematch/src/InTheaters.js
+++ cinnematch/src/InTheaters.js
@@ -1,15 +1,53 @@
 import React from 'react'
 
-class InTheaters extends React.Component{
+class Showtimes extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      json: {},
+      listView: [],
+      rendered: false
+    };
+    this.apiRequest = this.apiRequest.bind(this);
+  }  
+
+
+  populatePage() {
+    console.log(this.state.json);
+    this.setState({
+      rendered: true
+    })
+    return(
+      <div>
+        Funct ret
+      </div>
+    );
+  }
+
+  /**
+   * API request for current movies in theaters around the US
+   */
+  apiRequest() {
+    const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=d75d2433d1369590a08680adda987f45&language=en-US&page=1";
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(json => this.setState({ json }))
+      .then((populate) => this.populatePage())
+  }
+  
   render() {
     return(
       <div>
         <header className="w3-container w3-goldenrod">
-          <h1 className="Center">In Theaters</h1>
+          <h1 className="Center">Showtimes</h1>
         </header>
+        <div>
+          <button onClick={this.apiRequest()}>Whats in Theaters</button>
+        </div>
       </div>   
     );
   }
 }
 
-export default InTheaters;
+export default Showtimes;

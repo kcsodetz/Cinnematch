4c2cd73dad3ab86ee041299cc3d7fbcfe9d6f337
Connor Todd <todd37@purdue.edu>
2018-04-25 22:46:47 -0400

    Make two new files for list component

---
+++ cinnematch/src/ProfileDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ProfileDisplay.css'
+
+const ProfileDisplay = () => {
+
+  return (
+    <div>
+    <ul ref="movieList">
+
+    </ul>
+    </div>
+  )
+}
+export default ProfileDisplay
